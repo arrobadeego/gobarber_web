25caf2a27a2bd2fa50332c9795930e0b441f9af1
Diego Moreira <diegomptcc@hotmail.com>
2019-09-10 10:33:53 -0300

    create files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class App {
-    render() {
-        return <div className="App" />;
-    }
+function App() {
+    return <div className="App" />;
 }
 
 export default App;
