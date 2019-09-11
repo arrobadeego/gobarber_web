235c7f681dcd6d85c103bfca7adb69d2bbf46398
Diego Moreira <diegomptcc@hotmail.com>
2019-09-11 17:44:50 -0300

    create Header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import logo from '~/assets/logopurple.svg';
 import { Container, Content, Profile } from './styles';
 
-import logo from '~/assets/logopurple.svg';
-
 export default function Header() {
+    const profile = useSelector(state => state.user.profile);
+
     return (
         <Container>
             <Content>
                 <nav>
-                    <img src={logo} alt="gobarber" />
-                    <Link to="/dashboard">DASHBOARD</Link>
-
-                    <aside>
-                        <Profile>
-                            <div>
-                                <strong>Diego</strong>
-                                <Link to="/profile">Meu perfil</Link>
-                            </div>
-                            <img
-                                src="https://api.adorable.io/avatars/50/abott@adorable.png"
-                                alt="avatar"
-                            />
-                        </Profile>
-                    </aside>
+                    <img src={logo} alt="GoBarber" />
+                    <Link to="/dashboard">Dashboard</Link>
                 </nav>
+
+                <aside>
+                    <Profile>
+                        <div>
+                            <strong>Diego</strong>
+                            <Link to="/profile">Meu perfil</Link>
+                        </div>
+
+                        <img
+                            src="https://api.adorable.io/avatars/50/abott@adorable.png'"
+                            alt="Diego"
+                        />
+                    </Profile>
+                </aside>
             </Content>
         </Container>
     );
