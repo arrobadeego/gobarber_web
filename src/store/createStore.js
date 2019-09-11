28b6c7dcdbe7e18196c81b87bdd5568107c31b7d
Diego Moreira <diegomptcc@hotmail.com>
2019-09-11 09:57:54 -0300

    fix import problems

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,14 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { createStore, compose, applyMiddleware } from 'redux';
 
 export default (reducers, middlewares) => {
     const enhancer =
         process.env.NODE_ENV === 'development'
-        ? compose(
-            console.tron.createEnhancer(),
-            applyMiddleware(...middlewares))
-        : applyMiddleware(...middlewares);
+            ? compose(
+                  console.tron.createEnhancer(),
+                  applyMiddleware(...middlewares)
+              )
+            : applyMiddleware(...middlewares);
 
     return createStore(reducers, enhancer);
 };
