609f44d774553111d9207b44576363a09e241e3a
Diego Moreira <diegomptcc@hotmail.com>
2019-09-12 13:13:41 -0300

    create profile component

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-    return <h1>Profile</h1>;
+    return (
+        <Container>
+            <Form>
+                <Input name="name" placeholder="Seu nome completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu endereço de e-mail"
+                />
+
+                <hr />
+
+                <Input
+                    name="oldPassword"
+                    type="password"
+                    placeholder="Sua senha atual"
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua nova senha"
+                />
+                <Input
+                    name="confirmPassword"
+                    type="password"
+                    placeholder="Confirme sua senha"
+                />
+
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+
+            <button type="button">Sair do GoBarber</button>
+        </Container>
+    );
 }
