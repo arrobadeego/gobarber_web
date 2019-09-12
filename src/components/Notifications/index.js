32c9d94db48a337e067322ce29d843a7e3d2b967
Diego Moreira <diegomptcc@hotmail.com>
2019-09-12 08:41:40 -0300

    create Notifications component

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { MdNotifications } from 'react-icons/md';
+
+import { Container, Badge } from './styles';
+
 export default function Notifications() {
     return (
-        <div>
-
-        </div>
-    )
+        <Container>
+            <Badge>
+                <MdNotifications color="#7159c1" size={20} />
+            </Badge>
+        </Container>
+    );
 }
