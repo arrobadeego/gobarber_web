f862660b45d7a7c3f33c3288ac611c9d0d88a398
Diego Moreira <diegomptcc@hotmail.com>
2019-09-12 10:27:16 -0300

    hide button on all notifications are read

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -69,14 +69,16 @@ export default function Notifications() {
                         >
                             <p>{notification.content}</p>
                             <time>{notification.timeDistance}</time>
-                            <button
-                                onClick={() =>
-                                    handleMarkAsRead(notification._id)
-                                }
-                                type="button"
-                            >
-                                Marcar como lida
-                            </button>
+                            {!notification.read && (
+                                <button
+                                    onClick={() =>
+                                        handleMarkAsRead(notification._id)
+                                    }
+                                    type="button"
+                                >
+                                    Marcar como lida
+                                </button>
+                            )}
                         </Notification>
                     ))}
                 </Scroll>
